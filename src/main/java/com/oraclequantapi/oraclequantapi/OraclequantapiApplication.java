9dd10622247ad669593fea7420d47916d339b153
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2026-05-20 12:11:45 +0400

    Initialized and empty SpringBoot Project

---
+++ src/main/java/com/oraclequantapi/oraclequantapi/OraclequantapiApplication.java
@@ -0,0 +1,13 @@
+package com.oraclequantapi.oraclequantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraclequantapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OraclequantapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oraclequantapi/oraclequantapi/OraclequantapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oraclequantapi.oraclequantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OraclequantapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
