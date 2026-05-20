a87f5900175574b7baf1238e57f9c8132f551a9a
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-20 17:23:07 +0400

    Implemented continuation mode decoding

--- src/main/java/com/oraclequantapi/oraclequantapi/parser/DecodedNumber.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/parser/DecodedNumber.java
@@ -19,4 +19,3 @@ public class DecodedNumber {
         return nextIndex;
     }
 }
-
--- src/main/java/com/oraclequantapi/oraclequantapi/parser/MeasurementParser.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/parser/MeasurementParser.java
@@ -5,19 +5,42 @@ import java.util.List;
 public class MeasurementParser {
 
     public List<Integer> parseMeasurements(String input) {
+        return null;
     }
 
     private DecodedNumber readEncodedNumber(String input, int currentIndex) {
         char currentCharacter = input.charAt(currentIndex);
-        int value;
+        int value = 0;
 
-        if (currentCharacter == '_') {
-            value = 0;
-        } else {
-            value = (currentCharacter - 'a') + 1;
+        if (currentCharacter == 'z') {
+            while (currentIndex < input.length()
+                    && input.charAt(currentIndex) == 'z') {
+
+                value += 26;
+                currentIndex++;
+            }
+
+            if (currentIndex < input.length()) {
+                char terminatingCharacter = input.charAt(currentIndex);
+
+                if (terminatingCharacter == '_') {
+                    value += 0;
+                }
+                else {
+                    value += (terminatingCharacter - 'a') + 1;
+                }
+                currentIndex++;
+            }
         }
 
-        return new DecodedNumber(value, currentIndex + 1);
+        else {
+            if (currentCharacter == '_') {
+                value = 0;
+            } else {
+                value = (currentCharacter - 'a') + 1;
+            }
+            currentIndex++;
+        }
+        return new DecodedNumber(value, currentIndex);
     }
-
 }
