4af717b57327759777e11bf8b00f4dc7eaac1e57
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-05-21 11:50:24 +0400

    Initializing the table

--- src/main/java/com/oraclequantapi/oraclequantapi/models/MeasurementsHistory.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/models/MeasurementsHistory.java
@@ -1,8 +1,17 @@
 package com.oraclequantapi.oraclequantapi.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "MEASUREMENT_TABLE")
 public class MeasurementsHistory {
+
+    @Id
+    @Column(name = "ID", length = 36)
+    public String id;
+
 
 }
