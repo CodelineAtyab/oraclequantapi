b62f1eb5931e824adaf4c41ed25c78410b5fbf3c
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-05-21 12:20:48 +0400

    Updating second Controller class

--- src/main/java/com/oraclequantapi/oraclequantapi/controllers/HistoryController.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/controllers/HistoryController.java
@@ -1,10 +1,46 @@
 package com.oraclequantapi.oraclequantapi.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping (path = "/history")
 public class HistoryController {
+    @Autowired
+    public HistoryService historyService;
+
+    // Return all saved conversion requests from Oracle DB.
+    @GetMapping
+    public ResponseEntity<List<MeasurementHistory>> getAllHistory() {
+        return ResponseEntity.status(HttpStatus.OK).body(historyService.getHistory());
+    }
+
+    // Return one saved conversion request by id.
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<MeasurementHistory> getSpecificHistory(@PathVariable String id) {
+        MeasurementHistory history = historyService.getHistoryById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(history);
+    }
+
+    // Update a history record by id.
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<MeasurementHistory> updateHistory(
+            @PathVariable String id,
+            @RequestBody MeasurementHistory incomingHistory
+    ) {
+        MeasurementHistory history = historyService.updateHistory(id, incomingHistory);
+        return ResponseEntity.status(HttpStatus.OK).body(history);
+    }
+
+    // Delete all saved history records.
+    @DeleteMapping
+    public ResponseEntity<Void> clearHistory() {
+        historyService.clearHistory();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 }
