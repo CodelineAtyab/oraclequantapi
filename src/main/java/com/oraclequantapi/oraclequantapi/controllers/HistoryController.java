e2d7e7c456eaab23d6b5da4753b0dad79426ea3e
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-05-21 12:20:14 +0400

    Adding second Controller class

---
+++ src/main/java/com/oraclequantapi/oraclequantapi/controllers/HistoryController.java
@@ -0,0 +1,10 @@
+package com.oraclequantapi.oraclequantapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (path = "/history")
+public class HistoryController {
+
+}
