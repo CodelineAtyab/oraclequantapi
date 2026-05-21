105099a5671673da250dd5c29b9b8ecc72ca8ac4
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-21 11:51:46 +0400

    Disabled temporary datasource auto configuration

--- src/main/java/com/oraclequantapi/oraclequantapi/OraclequantapiApplication.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/OraclequantapiApplication.java
@@ -3,7 +3,11 @@ package com.oraclequantapi.oraclequantapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (
+		exclude = {
+				org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+		}
+)
 public class OraclequantapiApplication {
 
 	public static void main(String[] args) {
