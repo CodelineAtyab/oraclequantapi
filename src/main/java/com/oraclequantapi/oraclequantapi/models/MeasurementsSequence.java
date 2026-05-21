4a6c57e234e258521a0a763824df9380957b5372
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-05-21 11:23:27 +0400

    Updating condition statement to normalize the inputs

--- src/main/java/com/oraclequantapi/oraclequantapi/models/MeasurementsSequence.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/models/MeasurementsSequence.java
@@ -1,6 +1,7 @@
 package com.oraclequantapi.oraclequantapi.models;
 
-import static sun.security.krb5.internal.ktab.KeyTab.normalize;
+
+import java.util.Locale;
 
 public class MeasurementsSequence {
     
@@ -18,5 +19,21 @@ public class MeasurementsSequence {
         } else {
             this.value = normalize(value);
         }
+    }
+
+    private String normalize (String value){
+        String lowerValue = value.toLowerCase(Locale.ROOT);
+        StringBuilder normalizedValue = new StringBuilder();
+
+        for (int index = 0; index < lowerValue.length(); index++) {
+            char current = lowerValue.charAt(index);
+
+            if ((current >= 'a' && current <= 'z') || current == '_') {
+                normalizedValue.append(current);
+            }else {
+                normalizedValue.append('_');
+            }
+        }
+        return normalizedValue.toString();
     }
 }
