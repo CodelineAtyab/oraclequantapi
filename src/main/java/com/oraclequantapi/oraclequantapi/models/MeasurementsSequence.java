2e03e5a396ef5ce52ca0f57e25909da7795d11ec
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-05-21 11:06:27 +0400

    Adding value to hold the input

--- src/main/java/com/oraclequantapi/oraclequantapi/models/MeasurementsSequence.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/models/MeasurementsSequence.java
@@ -1,4 +1,13 @@
 package com.oraclequantapi.oraclequantapi.models;
 
 public class MeasurementsSequence {
+    
+    public String value;
+    
+    public MeasurementsSequence(String value){
+        setValue(value);
+    }
+
+    private void setValue(String value) {
+    }
 }
