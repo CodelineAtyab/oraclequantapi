febb8393893641c19866423fe0ab18960090b942
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-05-21 11:11:09 +0400

    Adding condition statement to normalize the inputs

--- src/main/java/com/oraclequantapi/oraclequantapi/models/MeasurementsSequence.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/models/MeasurementsSequence.java
@@ -1,5 +1,7 @@
 package com.oraclequantapi.oraclequantapi.models;
 
+import static sun.security.krb5.internal.ktab.KeyTab.normalize;
+
 public class MeasurementsSequence {
     
     public String value;
@@ -8,6 +10,13 @@ public class MeasurementsSequence {
         setValue(value);
     }
 
+    //  capital letters converted into lowercases
+    //  Anything than letters and underscore will be converted into underscore
     private void setValue(String value) {
+        if (value == null) {
+            this.value = null;
+        } else {
+            this.value = normalize(value);
+        }
     }
 }
