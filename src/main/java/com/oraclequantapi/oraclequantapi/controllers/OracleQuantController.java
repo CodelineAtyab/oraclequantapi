ef1ac56eac1410d44f2791c7bf07c85098264d02
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-05-21 09:56:34 +0400

    prepare basic controller functions

--- src/main/java/com/oraclequantapi/oraclequantapi/controllers/OracleQuantController.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/controllers/OracleQuantController.java
@@ -1,4 +1,35 @@
 package com.oraclequantapi.oraclequantapi.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/OracleQuant")
 public class OracleQuantController {
+
+    @GetMapping(path = "/Convert")
+    public List<String> Convert(@PathVariable String value) {
+        return null;
+    }
+
+    @GetMapping(path = "/Fetch")
+    public List<String> fetch(@PathVariable String value) {
+        return null;
+    }
+
+    @GetMapping(path = "/FetchAll")
+    public List<String> fetchAll() {
+        return null;
+    }
+
+    @PutMapping(path = "/Update")
+    public List<String> Update(@PathVariable String value) {
+        return null;
+    }
+
+    @DeleteMapping(path = "/Delete")
+    public List<String> delete(@PathVariable String value) {
+        return null;
+    }
 }
