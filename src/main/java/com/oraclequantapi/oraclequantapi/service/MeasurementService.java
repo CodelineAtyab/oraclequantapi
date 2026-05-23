1ee782c3a46ec33afce1adf45d6540bad59bec66
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-24 00:53:12 +0400

    Added measurement history service contract

--- src/main/java/com/oraclequantapi/oraclequantapi/service/MeasurementService.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/service/MeasurementService.java
@@ -1,7 +1,9 @@
 package com.oraclequantapi.oraclequantapi.service;
 
+import com.oraclequantapi.oraclequantapi.entity.MeasurementRecord;
 import java.util.List;
 
 public interface MeasurementService {
     List<Integer> convertMeasurements(String input);
+    List<MeasurementRecord> getMeasurementHistory();
 }
