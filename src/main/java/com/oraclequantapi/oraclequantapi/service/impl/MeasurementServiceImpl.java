1621ae46bb9c3a5736ddfe55a5da3f54b0147f88
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-23 21:50:10 +0400

    Added service logging infrastructure

--- src/main/java/com/oraclequantapi/oraclequantapi/service/impl/MeasurementServiceImpl.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/service/impl/MeasurementServiceImpl.java
@@ -2,12 +2,17 @@ package com.oraclequantapi.oraclequantapi.service.impl;
 
 import com.oraclequantapi.oraclequantapi.parser.MeasurementParser;
 import com.oraclequantapi.oraclequantapi.service.MeasurementService;
-
 import java.util.List;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class MeasurementServiceImpl implements MeasurementService {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(MeasurementServiceImpl.class);
 
     private final MeasurementParser measurementParser =
             new MeasurementParser();
