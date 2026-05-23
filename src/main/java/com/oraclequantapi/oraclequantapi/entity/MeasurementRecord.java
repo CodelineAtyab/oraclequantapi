21640118330ad5a9b0de79e2c4984e8949b46724
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-23 22:25:34 +0400

    Created measurement persistence entity

--- src/main/java/com/oraclequantapi/oraclequantapi/entity/MeasurementRecord.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/entity/MeasurementRecord.java
@@ -1,4 +1,39 @@
 package com.oraclequantapi.oraclequantapi.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class MeasurementRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String input;
+
+    private String output;
+
+    public MeasurementRecord() {
+
+    }
+
+    public MeasurementRecord(String input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
 }
