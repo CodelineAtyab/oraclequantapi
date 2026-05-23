1fb500e1464ed9c4e2297edae66610a49d7fd0c1
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-24 00:29:31 +0400

    Configured Oracle datasource integration

--- src/main/java/com/oraclequantapi/oraclequantapi/OraclequantapiApplication.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/OraclequantapiApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication (
-		exclude = {
-				org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-		}
+
 )
 public class OraclequantapiApplication {
 
--- src/main/java/com/oraclequantapi/oraclequantapi/service/impl/MeasurementServiceImpl.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/service/impl/MeasurementServiceImpl.java
@@ -4,6 +4,8 @@ import com.oraclequantapi.oraclequantapi.parser.MeasurementParser;
 import com.oraclequantapi.oraclequantapi.service.MeasurementService;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import com.oraclequantapi.oraclequantapi.entity.MeasurementRecord;
+import com.oraclequantapi.oraclequantapi.repository.MeasurementRecordRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,8 +16,18 @@ public class MeasurementServiceImpl implements MeasurementService {
     private static final Logger logger =
             LoggerFactory.getLogger(MeasurementServiceImpl.class);
 
+    private final MeasurementRecordRepository
+            measurementRecordRepository;
+
     private final MeasurementParser measurementParser =
             new MeasurementParser();
+
+    public MeasurementServiceImpl(
+            MeasurementRecordRepository measurementRecordRepository
+    ) {
+        this.measurementRecordRepository =
+                measurementRecordRepository;
+    }
 
     @Override
     public List<Integer> convertMeasurements(String input) {
