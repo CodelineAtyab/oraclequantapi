b879431f5f53b2cc25312e06bace1e31436b8a98
safa <safa@safas-MacBook-Air.local>
2026-05-23 12:12:44 +0400

    creste historyservice class

---
+++ src/main/java/com/oraclequantapi/oraclequantapi/service/HistoryService.java
@@ -0,0 +1,52 @@
+package com.oraclequantapi.oraclequantapi.service;
+import com.oraclequantapi.oraclequantapi.MeasurementRecord;
+import com.oraclequantapi.oraclequantapi.MeasurementRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class HistoryService {
+
+    @Autowired
+    private MeasurementRecordRepository measurementRecordRepository;
+
+    @Autowired
+    private MeasurementParser measurementParser;
+
+    public MeasurementRecord save(String input, String sourceIp) {
+        List<Long> output = measurementParser.parse(input);
+        MeasurementRecord record = new MeasurementRecord(
+                0,
+                LocalDateTime.now(),
+                sourceIp,
+                input,
+                output
+        );
+        return measurementRecordRepository.save(record);
+    }
+
+    public List<MeasurementRecord> getAll() {
+        return measurementRecordRepository.findAll();
+    }
+
+    public Optional<MeasurementRecord> getById(long id) {
+        return measurementRecordRepository.findById(id);
+    }
+
+    public Optional<MeasurementRecord> update(long id, String newInput) {
+        Optional<MeasurementRecord> record = measurementRecordRepository.findById(id);
+        record.ifPresent(r -> {
+            r.setInput(newInput);
+            r.setOutput(measurementParser.parse(newInput));
+            measurementRecordRepository.save(r);
+        });
+        return record;
+    }
+
+    public void clearAll() {
+        measurementRecordRepository.deleteAll();
+    }
+}
