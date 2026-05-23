265fe4d4b27c3fca163841b64ee80c5f61a1ed19
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-23 21:21:39 +0400

    Implemented global API exception handling

--- src/main/java/com/oraclequantapi/oraclequantapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/exception/GlobalExceptionHandler.java
@@ -1,4 +1,28 @@
 package com.oraclequantapi.oraclequantapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>>
+    handleIllegalArgumentException(
+            IllegalArgumentException exception
+    ) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        Map.of(
+                                "error",
+                                exception.getMessage()
+                        )
+                );
+    }
 }
