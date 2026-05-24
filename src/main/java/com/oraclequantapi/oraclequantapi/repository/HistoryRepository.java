b0c983b21278a4cc02763a493c9125f33aad172a
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-24 20:14:24 +0400

    Extend JpaRepository for HistoryRecord entity

--- src/main/java/com/oraclequantapi/oraclequantapi/repository/HistoryRepository.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/repository/HistoryRepository.java
@@ -3,5 +3,5 @@ package com.oraclequantapi.oraclequantapi.repository;
 import com.oraclequantapi.oraclequantapi.model.HistoryRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HistoryRepository {
+public interface HistoryRepository extends JpaRepository<HistoryRecord, Long> {
 }
