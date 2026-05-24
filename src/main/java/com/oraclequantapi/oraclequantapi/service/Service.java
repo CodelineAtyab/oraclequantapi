0886278a595c8deed985bb612bbab1381fc1e8bf
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-24 10:12:25 +0400

    Add Service class

---
+++ src/main/java/com/oraclequantapi/oraclequantapi/service/Service.java
@@ -0,0 +1,13 @@
+package com.oraclequantapi.oraclequantapi.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Service {
+
+    private final List<Sequence> sequenceList = new ArrayList<>();
+
+}
