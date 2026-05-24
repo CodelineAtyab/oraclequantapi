fc26d7631a733d7861b5cf43dba55524f125efd6
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-25 01:02:24 +0400

    Add REST controller and request mapping annotations

--- src/main/java/com/oraclequantapi/oraclequantapi/controller/HistoryController.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/controller/HistoryController.java
@@ -1,4 +1,9 @@
 package com.oraclequantapi.oraclequantapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/history")
 public class HistoryController {
 }
