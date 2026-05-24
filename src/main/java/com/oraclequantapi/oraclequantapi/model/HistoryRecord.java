af3b81c2d9a63ba1aaa439d50d929692a633b2aa
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-24 19:45:23 +0400

    Add JPA entity and table mapping

--- src/main/java/com/oraclequantapi/oraclequantapi/model/HistoryRecord.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/model/HistoryRecord.java
@@ -1,4 +1,9 @@
 package com.oraclequantapi.oraclequantapi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "CONVERSION_HISTORY")
 public class HistoryRecord {
 }
