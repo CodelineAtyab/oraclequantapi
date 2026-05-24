33434381cadb5bd515b2ac2476367ec37fc90e00
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-24 10:12:36 +0400

    Implement add sequence

--- src/main/java/com/oraclequantapi/oraclequantapi/service/Service.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/service/Service.java
@@ -10,4 +10,10 @@ public class Service {
 
     private final List<Sequence> sequenceList = new ArrayList<>();
 
+    // Stores enquiry and returns it with server-generated id and timestamp
+    public Sequence addSequence(Sequence sequence) {
+        sequenceList.add(sequence);
+        return sequence;
+    }
+
 }
