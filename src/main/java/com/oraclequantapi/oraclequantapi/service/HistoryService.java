9e466de2a488a51abfe9ec2e5da689d837e629ee
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-25 00:15:14 +0400

    Add repository and object mapper dependencies

--- src/main/java/com/oraclequantapi/oraclequantapi/service/HistoryService.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/service/HistoryService.java
@@ -1,7 +1,11 @@
 package com.oraclequantapi.oraclequantapi.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oraclequantapi.oraclequantapi.repository.HistoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HistoryService {
+    private final HistoryRepository repository;
+    private final ObjectMapper objectMapper;
 }
