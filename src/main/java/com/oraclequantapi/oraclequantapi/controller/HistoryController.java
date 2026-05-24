8b433f4f37b5e3e7b7ad7b9759bb5692d419aee8
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-25 01:03:47 +0400

    Add HistoryService dependency

--- src/main/java/com/oraclequantapi/oraclequantapi/controller/HistoryController.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/controller/HistoryController.java
@@ -1,5 +1,6 @@
 package com.oraclequantapi.oraclequantapi.controller;
 
+import com.oraclequantapi.oraclequantapi.service.HistoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,4 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/history")
 public class HistoryController {
     private static final Logger log = LoggerFactory.getLogger(HistoryController.class);
+    private final HistoryService historyService;
 }
