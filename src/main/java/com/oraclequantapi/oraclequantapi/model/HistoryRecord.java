270f70fc7cdeb215c5684aad95b8a65041e391eb
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-24 19:47:01 +0400

    Add primary key field

--- src/main/java/com/oraclequantapi/oraclequantapi/model/HistoryRecord.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/model/HistoryRecord.java
@@ -1,9 +1,13 @@
 package com.oraclequantapi.oraclequantapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "CONVERSION_HISTORY")
 public class HistoryRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
 }
