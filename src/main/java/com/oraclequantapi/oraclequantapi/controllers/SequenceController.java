340c746a2e506d766ac4340cf28e8eb87e1228b5
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-24 22:38:38 +0400

    controller and service files added, must connect db and auto increment id value and work on ip

--- src/main/java/com/oraclequantapi/oraclequantapi/controllers/SequenceController.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/controllers/SequenceController.java
@@ -1,4 +1,27 @@
 package com.oraclequantapi.oraclequantapi.controllers;
 
+
+import com.oraclequantapi.oraclequantapi.models.Sequence;
+import com.oraclequantapi.oraclequantapi.services.SequenceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class SequenceController {
+
+    private final SequenceService sequenceService;
+
+    public SequenceController(SequenceService sequenceService) {
+        this.sequenceService = sequenceService;
+    }
+
+    @GetMapping("/convert-measurements")
+    public ResponseEntity<List<String>> convertMeasurements(@RequestParam String input) {
+        Sequence sequence = sequenceService.decode(input);
+        return ResponseEntity.ok(sequence.getValue());
+    }
 }
