cf27895818581e73d1ac295dc936c1e2d4bb3fd8
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-25 00:44:13 +0400

    Add convert endpoint method

--- src/main/java/com/oraclequantapi/oraclequantapi/controller/MeasurementController.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/controller/MeasurementController.java
@@ -2,9 +2,14 @@ package com.oraclequantapi.oraclequantapi.controller;
 
 import com.oraclequantapi.oraclequantapi.service.HistoryService;
 import com.oraclequantapi.oraclequantapi.service.MeasurementService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MeasurementController {
@@ -16,6 +21,12 @@ public class MeasurementController {
     public MeasurementController(MeasurementService measurementService, HistoryService historyService) {
         this.measurementService = measurementService;
         this.historyService = historyService;
+    }
+
+    @GetMapping("/convert-measurements")
+    public List<Integer> convert(@RequestParam(required = false) String input, HttpServletRequest request) {
+
+        return List.of();
     }
 
 }
