b5aaa132dcd90a2a5b89bb0ff3a918f66d7590b4
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-24 23:16:06 +0400

    Add convert method structure

--- src/main/java/com/oraclequantapi/oraclequantapi/service/MeasurementService.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/service/MeasurementService.java
@@ -2,6 +2,12 @@ package com.oraclequantapi.oraclequantapi.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MeasurementService {
+
+    public List<Integer> convert(String input) {
+        return List.of();
+    }
 }
