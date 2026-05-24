1292d8375bdbd49686d450ad1583fe7d13e322ef
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-25 00:42:14 +0400

    Add service dependencies to controller

--- src/main/java/com/oraclequantapi/oraclequantapi/controller/MeasurementController.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/controller/MeasurementController.java
@@ -1,5 +1,7 @@
 package com.oraclequantapi.oraclequantapi.controller;
 
+import com.oraclequantapi.oraclequantapi.service.HistoryService;
+import com.oraclequantapi.oraclequantapi.service.MeasurementService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MeasurementController {
     private static final Logger log = LoggerFactory.getLogger(MeasurementController.class);
+
+    private final MeasurementService measurementService;
+    private final HistoryService historyService;
+
 }
