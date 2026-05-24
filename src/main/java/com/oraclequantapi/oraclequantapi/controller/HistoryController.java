1bb519f926efddd5ce95827fe929265eb13165a8
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-25 01:03:20 +0400

    Initialize controller logger

--- src/main/java/com/oraclequantapi/oraclequantapi/controller/HistoryController.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/controller/HistoryController.java
@@ -1,9 +1,12 @@
 package com.oraclequantapi.oraclequantapi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/history")
 public class HistoryController {
+    private static final Logger log = LoggerFactory.getLogger(HistoryController.class);
 }
