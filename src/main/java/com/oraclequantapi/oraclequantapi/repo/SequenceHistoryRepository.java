2c19989a9dff600b3e860ef65ad4ca2097384194
KhadijaAli <alloughanikhadijaali@gmail.com>
2026-05-25 22:49:26 +0400

    SequenceHistoryRepository

--- src/main/java/com/oraclequantapi/oraclequantapi/repo/SequenceHistoryRepository.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/repo/SequenceHistoryRepository.java
@@ -1,4 +1,10 @@
 package com.oraclequantapi.oraclequantapi.repo;
 
-public interface SequenceHistoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.oraclequantapi.oraclequantapi.model.SequenceHistory;
+
+@Repository
+public interface SequenceHistoryRepository extends JpaRepository<SequenceHistory, Long> {
+
 }
